2a504e9803a28c7a43805be0b3648af7a70521d8
Sp4ceFr4nk <enrico.franco99@gmail.com>
2022-12-31 23:23:54 +0100

    Added shortcut '\\' to disable higlight after search with '/'

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,18 @@
+-- require other config files
 require("plugins")
 require("plugin_config")
 
+-- Peronal shortcuts
+vim.keymap.set('n', '\\', ':noh<return><esc>')
+
+-- for easier syntax
 local set = vim.opt
 
-set.number = true
-set.tabstop = 2
+set.number = true -- show number on left side
+set.tabstop = 2 -- size of tab
 set.shiftwidth = 2
-set.expandtab = true
+set.expandtab = true -- expand tab on files
 
-set.cursorline = true
-set.scrolloff = 5
-set.showcmd = true
+set.cursorline = true -- highlight line with cursor
+set.scrolloff = 5 -- stop 5 lines before start scrolling down
+set.showcmd = true -- show commands
