0648deb42af8f06b89d240b0cc8e489a70eaecdc
Sp4ceFr4nk <enrico.franco99@gmail.com>
2022-12-31 23:23:30 +0100

    Modified lsp_config, lualine only aestethic, telescope's shortcuts and treesitter

--- nvim/lua/plugin_config/lualine.lua
+++ nvim/lua/plugin_config/lualine.lua
@@ -2,13 +2,15 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'onedark',
+    component_separators = { left = '|', right = '|'},
+    section_separators = { left = '', right = ''},
   },
   sections = {
     lualine_a = {
-      {
-	'filename',
-	path = 1,
-      }
-    }
+      {'filename',path = 1,}
+    },
+    lualine_b = {'branch', 'diff'},
+    lualine_c = {}, -- remove redondant filename
+    lualine_x = {'encoding', 'filetype'},
   }
 }
--- nvim/lua/plugin_config/treesitter.lua
+++ nvim/lua/plugin_config/treesitter.lua
@@ -1,9 +1,14 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "rust", "python", "html", "css", "javascript" },
+  ensure_installed = { -- languages to install
+    "c", "cpp", "lua", "rust", "python", "html", "css", "javascript", "typescript", "go"
+  },
   sync_install = false,
   auto_install = true,
-  highlight = {
+  highlight = { -- highlighting provided by treesitter
     enable = true,
-    additional_vim_regex_highlighting = false,
+    -- additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
   },
 }
