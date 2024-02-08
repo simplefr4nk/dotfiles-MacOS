a11d43b0858fc1a8c6855ad095dbbec1aaf83ef0
SimpleFr4nk <enrico.franco99@gmail.com>
2024-02-08 19:30:05 +0100

    Removed loader from vscode

--- .config/nvim/lua/plugin_config/completions.lua
+++ .config/nvim/lua/plugin_config/completions.lua
@@ -1,6 +1,6 @@
 local cmp = require('cmp')
 
-require('luasnip.loaders.from_vscode').lazy_load()
+--require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
