6e9bc8de9da8c31e3720e6832d738096e6ba282d
SimpleFr4nk <enrico.franco99@gmail.com>
2024-02-08 19:31:15 +0100

    Removed old unused lsp and added the new ones
    Changed the capabilities configuration too, made them similar to the wiki of mason lspconfig

--- .config/nvim/lua/plugin_config/_lsp_config.lua
+++ .config/nvim/lua/plugin_config/_lsp_config.lua
@@ -1,40 +1,23 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
   ensure_installed = {
-    "sumneko_lua",
-    "arduino_language_server",
-    "clangd",
-    "quick_lint_js",
-    "ltex",
-    "marksman",
-    "pylsp",
-    "rust_analyzer",
+    "lua_ls", -- lua
+    "marksman", -- markdown
+    "pylsp", -- python
+    "rust_analyzer", -- rust
   }
 })
 
+-- Add additional capabilities supported by nvim-cmp
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").sumneko_lua.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").arduino_language_server.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").clangd.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").quick_lint_js.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").ltex.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").marksman.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").pylsp.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").rust_analyzer.setup {
-  capabilities = capabilities,
-}
+
+local lspconfig = require('lspconfig')
+
+local servers = { 'rust_analyzer', 'pylsp', 'rust_analyzer', 'marksman', 'lua_ls' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
--- .config/nvim/lua/plugin_config/lsp_config.lua
+++ .config/nvim/lua/plugin_config/lsp_config.lua
@@ -49,18 +49,6 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  clangd = {},
-  gopls = {},
-  pyright = {},
-  rust_analyzer = {},
-  tsserver = {},
-
-  sumneko_lua = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
 }
 
 -- Setup neovim lua configuration
